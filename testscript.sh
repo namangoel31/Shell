5594681562597bacbcfe52efe002b0109d536cc6
Naman Goel <43837926+namangoel31@users.noreply.github.com>
2021-02-18 01:53:21 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -3,3 +3,5 @@ echo "You are in second shell!"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
+aws s3 cp s3://batchawsbucket/DbAccess/testcode.py /home/Shell/testcode.py
+python3 /home/Shell/testcode.py
