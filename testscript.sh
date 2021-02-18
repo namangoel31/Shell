adfd33aff15a74648d8cb979d8ea4faed00425c1
Naman Goel <43837926+namangoel31@users.noreply.github.com>
2021-02-18 14:39:23 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -5,3 +5,4 @@ unzip awscliv2.zip
 ./aws/install
 aws s3 cp s3://batchawsbucket/DbAccess/testcode.py /home/Shell/testcode.py
 python3 /home/Shell/testcode.py
+aws s3 cp /home/Shell/testfile.txt s3://batchawsbucket/DbAccess/testfile.txt
