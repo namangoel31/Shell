07e236e6a22720f7b27f2c9bb94d4b0b720d47f7
Naman Goel <43837926+namangoel31@users.noreply.github.com>
2021-02-19 11:26:17 +0530

    Create testscript.sh

---
+++ testscript.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "You are in second shell!"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+aws s3 cp $BUCKET_URL/testcode.py /home/Shell/testcode.py
+python3 /home/Shell/testcode.py
+aws s3 cp /home/Shell/testfile.txt $BUCKET_URL/testfile.txt
