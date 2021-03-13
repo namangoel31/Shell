0eaa7058199b5db6c147f0f52588fdd155cdf4eb
Naman Goel <43837926+namangoel31@users.noreply.github.com>
2021-03-14 04:37:39 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -4,5 +4,5 @@ aws s3 cp $BUCKET_URL/testcode.py /home/testcode.py
 mkdir templates
 aws s3 cp $BUCKET_URL/templates/index.html /home/templates/index.html
 aws s3 cp $BUCKET_URL/wallstreet.csv /home/wallstreet.csv
-python3 /home/Shell/testcode.py
+python3 /home/testcode.py
 echo "second shell complete"
