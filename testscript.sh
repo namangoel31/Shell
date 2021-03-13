3d5fdb6a428aaf568f7fec4314d92a83433bf3c2
Naman Goel <43837926+namangoel31@users.noreply.github.com>
2021-03-14 04:30:55 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 echo "You are in second shell!"
-aws s3 cp $BUCKET_URL/testcode.py /home/Shell/testcode.py
+aws s3 cp $BUCKET_URL/testcode.py /home/testcode.py
+mkdir templates
+aws s3 cp $BUCKET_URL/templates/index.html /home/templates/index.html
+aws s3 cp $BUCKET_URL/wallstreet.csv /home/wallstreet.csv
 python3 /home/Shell/testcode.py
 echo "second shell complete"
