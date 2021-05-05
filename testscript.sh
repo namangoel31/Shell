4871eac748f782b49b87c3c97a45ca1cf2fa3d42
Naman Goel <43837926+namangoel31@users.noreply.github.com>
2021-05-05 10:51:00 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,3 +1,4 @@
 aws s3 cp $BUCKET_URL/testcode.py /home/Shell/testcode.py
+aws s3 cp $BUCKET_URL/dataset.csv /home/Shell/dataset.csv
 python3 /home/Shell/testcode.py
 aws s3 cp /home/Shell/out.zip $BUCKET_URL/out.zip
